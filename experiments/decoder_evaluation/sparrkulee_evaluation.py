617d4e6bdfd3d76b4aef6dbe8198b901d601be6e
Mike-boop <mdt20@ic.ac.uk>
2023-11-28 17:57:57 +0000

    results/evaluation - wip

---
+++ experiments/decoder_evaluation/sparrkulee_evaluation.py
@@ -0,0 +1,85 @@
+import os
+import glob
+import json
+import pickle
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    config_path = '/home/mdt20/Code/match-mismatch-decoders-ojsp-2023/config.json'
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+
+    # load composite decoder LDA weights
+    composite_decoder_path = os.path.join(os.path.join(config['root_results_dir'], "composite_decoder"))
+    with open(composite_decoder_path, 'rb') as f:
+        clf = pickle.load(f)
+
+    averaged_env_outputs_dir = os.path.join(os.path.join(config['root_results_dir'], "averaged_outputs_sparrkulee", "env"))
+    averaged_ffr_outputs_dir = os.path.join(os.path.join(config['root_results_dir'], "averaged_outputs_sparrkulee", "ffr", "composite_decoder.pkl"))
+    results_savedir = os.path.join(os.path.join(config['root_results_dir'], 'decoder_evaluation_results'))
+
+    # calculate accuracies for test split, heldout split
+    n_avg = 100 # number of averaged decoders we are going to use
+
+    for split in ['test', 'heldout']:
+
+        results_dict = {}
+
+        for sub in range(1, 86):
+            
+            
+            results_dict[f'sub-{sub:03d}'] = {
+                'envelope': {'predicted_labels':[], 'targets':[]},
+                'ffr': {'predicted_labels':[], 'targets':[]},
+                }
+            if split == 'heldout':
+                results_dict[f'sub-{sub:03d}'].update({
+                'composite': {'predicted_labels':[], 'targets':[]},
+                })
+
+
+            averaged_env_outputs_files = glob.glob(
+                os.path.join(averaged_env_outputs_dir, f'env_-_avg-{n_avg:03d}_-_{split}_-_sub-{sub:03d}*outputs.npz')
+                )
+            averaged_ffr_outputs_files = list(map(lambda x: x.replace('env', 'ffr'), averaged_env_outputs_files))
+
+
+            for i in range(len(averaged_env_outputs_files)):
+
+                env_data = np.load(averaged_env_outputs_files[i])
+                ffr_data = np.load(averaged_ffr_outputs_files[i])
+
+                env_predictions = env_data['predictions']
+                ffr_predictions = ffr_data['predictions']
+                targets = env_data['targets']
+                max_len = min(len(env_predictions, ffr_predictions))
+
+                results_dict[f'sub-{sub:03d}']['envelope']['predicted_labels'].append(env_predictions[:max_len]>0)
+                results_dict[f'sub-{sub:03d}']['envelope']['targets'].append(env_data['targets'])
+                results_dict[f'sub-{sub:03d}']['ffr']['predicted_labels'].append(ffr_predictions[:max_len]>0)
+                results_dict[f'sub-{sub:03d}']['ffr']['targets'].append(env_data['targets'])
+
+                if split == 'heldout':
+                    lda_features = np.vstack([env_predictions[:max_len], ffr_predictions[:max_len]]).T
+                    results_dict[f'sub-{sub:03d}']['composite']['predicted_labels'].append(clf.predict(lda_features).squeeze())
+                    results_dict[f'sub-{sub:03d}']['composite']['targets'].append(env_data['targets'])
+
+            
+            for decoder_key in results_dict[f'sub-{sub:03d}']:
+                
+                hits = np.concatenate(results_dict[f'sub-{sub:03d}'][decoder_key]['predicted_labels']) == \
+                    np.concatenate(results_dict[f'sub-{sub:03d}'][decoder_key]['targets'])
+                
+                n_observations = len(hits)
+                accuracy = hits.sum()/len(hits)
+
+                results_dict[f'sub-{sub:03d}'][decoder_key] = {
+                    'n_obs': n_observations,
+                    'acc': accuracy
+                }
+
+            
+        with open(os.path.join(results_savedir, f'sparrkulee_{split}_results.json')) as f:
+            json.dump(results_dict, f, indent=4)
